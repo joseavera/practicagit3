63bf1bc122b89e20bdcc5c254c4735fc6d025b42
joseavera <javera135@gmail.com>
2026-03-14 12:43:35 -0600

    Agregue el metodo saludar

--- Persona.cs
+++ Persona.cs
@@ -1,1 +1,8 @@
-public class Persona { }
+public class Persona { 
+
+    public string Nombre {get; set;}
+    public void Saludar()
+    {
+        Console.WriteLine("Soy un apersona en C#");
+    }
+}
